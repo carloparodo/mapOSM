e9cf28e66823fdcb5d9a473092f3cebddf3d4941
Alberto Farci <tarcisio.albi@gmail.com>
2019-10-12 22:13:55 +0200

    added logic rules for toggle in filter page

--- src/app/filters/filters.module.ts
+++ src/app/filters/filters.module.ts
@@ -2,13 +2,14 @@ import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FiltersPage } from './filters.page';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
+    ReactiveFormsModule,
     FormsModule,
     RouterModule.forChild([{ path: '', component: FiltersPage }])
   ],
