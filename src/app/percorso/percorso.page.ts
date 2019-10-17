6c2a831639e1941923503e49a15441038a4497c5
Alberto Farci <tarcisio.albi@gmail.com>
2019-10-17 08:46:27 +0200

    added logic to handle current position

--- src/app/percorso/percorso.page.ts
+++ src/app/percorso/percorso.page.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Platform } from '@ionic/angular';
+import { SQLite } from '@ionic-native/sqlite/ngx';
 
 @Component({
     selector: 'app-percorso',
     templateUrl: 'percorso.page.html'
   })
   export class PercorsoPage {
+      constructor(
+        public route: ActivatedRoute,
+        public plt: Platform,
+        private sqlite: SQLite,
+        private toast: Toast
+        ){
+        this.route.params.subscribe(params => {
+          console.log(params.id)
+          params.id; // --> Name must match wanted parameter
+        });
+      }
+
       
+  ionViewDidEnter(){
+
+    this.plt.ready().then(() => {
+      this.pathsSaved = []
+      this.sqlite.create({
+        name: 'filters.db',
+        location: 'default'
+      })
+        .then((db: SQLiteObject) => {
+          
+          db.executeSql(`select * from paths`,[])
+          .then((tableSelect)=>{
+            
+            if (tableSelect.rows.length > 0) {
+              
+              for (var i = 0; i < tableSelect.rows.length; i++) {
+                
+                this.pathsSaved.push(tableSelect.rows.item(i))
+              }
+            }
+            
+          })
+          .catch((e) => {
+            this.toast.show(JSON.stringify(e), '3000', 'center').subscribe(
+              toast => {
+                console.log(toast);
+            })
+          })
+        })
+    });
   }
   
